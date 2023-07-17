3cbb5caac973a5aac6a17f4c582a77741f11a272
Amiodas <amio.das.dip@gmail.com>
2023-07-17 10:29:03 +0100

    #toy details ui fix

--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -19,30 +19,45 @@ const ToyDetails = () => {
     console.log(id);
   };
   return (
-    <div>
-      <h2 className="text-2xl">Details</h2>
-      <div>
-        <div>
-          <img src={picture} alt="" />
+    <div className="mt-16 mb-16">
+      <div className="grid grid-cols-2 gap-10">
+        <div className="p-16">
+          <div className="flex justify-center">
+            <img src={picture} alt="" className="w-1/2" />
+          </div>
         </div>
         <div>
-          <h2>{name}</h2>
-          <h2>Price: ${price}</h2>
-          <h2>Description: {description}</h2>
-          <h2>Categories: {categories}</h2>
-          <h2>Subcategories: {subcategories}</h2>
-          <h2>Rating: {rating}</h2>
-          <h2>Seller: {seller}</h2>
-        </div>
-        <div>
-          <Link to={`/checkout/${_id}`}>
-            <button
-              onClick={() => handleBookToy(_id)}
-              className="btn btn-primary"
-            >
-              Checkout
-            </button>
-          </Link>
+          <div>
+            <h2 className="text-2xl font-bold text-primary">{name}</h2>
+            <p className="text-xl text-primary mt-6 mb-2 font-bold">
+              Price: ${price}
+            </p>
+            <p className="text-gray-600 mb-2">
+              <span className="font-bold">Categories:</span> {categories}
+            </p>
+            <p className="text-gray-600 mb-2">
+              <span className="font-bold">Subcategories:</span> {subcategories}
+            </p>
+            <p className="text-gray-600 mb-2">
+              <span className="font-bold">Rating:</span> {rating}
+            </p>
+            <p className="text-gray-600 mb-2">
+              <span className="font-bold">Seller:</span> {seller}
+            </p>
+            <p className="text-gray-600">
+              <span className="font-bold">Description:</span> {description}
+            </p>
+          </div>
+          <div className="mt-8">
+            <Link to={`/checkout/${_id}`}>
+              <button
+                onClick={() => handleBookToy(_id)}
+                className="btn btn-block btn-primary"
+              >
+                Checkout
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
